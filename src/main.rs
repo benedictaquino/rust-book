9f5e36796aaa5b8c8b0d2fe758b3e9b244348126
Benedict Aquino <benaq9@gmail.com>
2019-06-21 19:10:08 -0400

    refactored everything

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,57 @@
 use std::io;
 
 fn main() {
-    loop {
-        println!("Choose option:");
-        println!("1: Fahrenheit to Celsius");
-        println!("2: Celsius to Fahrenheit");
-        println!("0: Quit");
-
-        let mut option = String::new();
-
-        io::stdin().read_line(&mut option)
-            .expect("Failed to read line");
-
-        let option: u8 = match option.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        if option > 2 {
-            continue
-        } else if option != 0 {
-            convert_temperature(&option)
-        }
-        break;
-    }
-}
-
-fn convert_temperature(option: &u8) {
     let mut temperature: (f64, char) = (32.0, 'F');
     let new_temperature: (f64, char);
 
     loop {
         println!("Please enter temperature:");
 
-        let mut temperature_string = String::new();
+        let mut degrees = String::new();
 
-        io::stdin().read_line(&mut temperature_string)
+        io::stdin().read_line(&mut degrees)
             .expect("Failed to read line");
 
-        temperature.0 = match temperature_string.trim().parse() {
+        temperature.0 = match degrees.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        if *option == 1 {
-            temperature.1 = 'F';
-            new_temperature = ((temperature.0 - 32.0) * 5.0 / 9.0, 'C');
-            break;
-        } else if *option == 2 {
-            temperature.1 = 'C';
-            new_temperature = (temperature.0 * 9.0 / 5.0 + 32.0, 'F');
-            break;
-        }
+        break;
     }
+
+    loop {
+        println!("[F]ahrenheit or [C]elsius?");
+
+        let mut units = String::new();
+
+        io::stdin().read_line(&mut units)
+            .expect("Failed to read line");
+
+        temperature.1 = match units.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        break;
+    }
+
+    new_temperature = convert_temperature(temperature);
 
     println!("{}°{} = {}°{}.",
              temperature.0, temperature.1,
              new_temperature.0, new_temperature.1);
+}
+
+fn convert_temperature(temperature: (f64, char)) -> (f64, char) {
+    let new_temperature: (f64, char) = if temperature.1 == 'F' {
+        ((temperature.0 - 32.0) * 5.0 / 9.0, 'C')
+    } else if temperature.1 == 'C' {
+        (temperature.0 * 9.0 / 5.0 + 32.0, 'F')
+    } else {
+        temperature  // return same temperature if not F or C
+    };
+
+    return new_temperature; 
 }
 
