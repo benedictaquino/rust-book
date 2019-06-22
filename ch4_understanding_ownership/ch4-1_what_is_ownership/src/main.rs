9541a5f8f51f210bb0e29375842287836d4121aa
Benedict Aquino <benaq9@gmail.com>
2019-06-22 14:13:38 -0400

    Ownership and Functions
    Listing 4-3: Functions with ownership and scope annotated

--- ch4_understanding_ownership/ch4-1_what_is_ownership/src/main.rs
+++ ch4_understanding_ownership/ch4-1_what_is_ownership/src/main.rs
@@ -1,7 +1,23 @@
-#![allow(unused_variables)]
 fn main() {
-    let x = 5;
-    let y = x;
+    let s = String::from("hello");  // s comes into scope
 
-    println!("x = {}, y = {}", x , y);
-}
+    takes_ownership(s);             // s's value moves into the function ...
+                                    // ... and is no longer valid here
+
+    let x = 5;                      // x comes into scope
+    
+    makes_copy(x);                  // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
+
+} // Here, x goes out of scope, then s. But because s's value was moved, noting
+  // special happens
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here some_string goes out of scope and `drop` is called. The backing
+  // memory is freed
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here some_integer goes out of scope. Nothing special happens.
