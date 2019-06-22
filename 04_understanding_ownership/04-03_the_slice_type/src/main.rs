c1fddc118b7053715515256e8dc848c559af88de
Benedict Aquino <benaq9@gmail.com>
2019-06-22 16:31:34 -0400

    String Slices
    wrote `second_word` function

--- 04_understanding_ownership/04-03_the_slice_type/src/main.rs
+++ 04_understanding_ownership/04-03_the_slice_type/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     let some_string = String::from("hello world");
-    let word = first_word(&some_string);
-    println!("The first word in '{}' is '{}'.", some_string, word);
+    let word1 = first_word(&some_string);
+    let word2 = second_word(&some_string);
+    println!("The first word in '{}' is '{}'.", some_string, word1);
+    println!("The second word in '{}' is '{}'.", some_string, word2);
 }
 
 fn first_word(s: &String) -> &str {
@@ -15,3 +17,21 @@ fn first_word(s: &String) -> &str {
 
     &s[..]
 }
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut start = 0;
+    let mut end = s.len();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if start == 0 && item == b' ' {
+            start = i + 1;
+        } else if item == b' ' {
+            end = i;
+            break;
+        }
+    }
+
+    &s[start..end]
+}
+
