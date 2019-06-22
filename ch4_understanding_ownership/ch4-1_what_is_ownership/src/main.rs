e4ee281be8e25ebabf5e4e892568cdbff66a656c
Benedict Aquino <benaq9@gmail.com>
2019-06-22 12:53:07 -0400

    variable scope

---
+++ ch4_understanding_ownership/ch4-1_what_is_ownership/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {           // s is not valid here, it's not yet declared
+    let s = "hello";  // s is valid from this point forward
+
+    // do stuff with s
+}                     // this scope is now over, and is is no longer valid
