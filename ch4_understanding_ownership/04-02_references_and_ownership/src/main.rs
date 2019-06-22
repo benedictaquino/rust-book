9c1624b26f7ec1de3effb7c6bcee0689e24ddb3f
Benedict Aquino <benaq9@gmail.com>
2019-06-22 15:51:05 -0400

    Dangling References
    create a dangling references

--- ch4_understanding_ownership/04-02_references_and_ownership/src/main.rs
+++ ch4_understanding_ownership/04-02_references_and_ownership/src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    let mut s = String::from("hello");
+    let reference_to_nothing = dangle();
+}
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{}, {}", r1, r2);
-    // r1 and r2 are no longer used after this point
+fn dangle() -> &String {
+    let s = String::from("hello");
 
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
+    &s
 }
 
