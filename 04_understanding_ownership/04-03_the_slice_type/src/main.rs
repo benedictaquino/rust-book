4cefbf6978d0e56b5d6be94a39fe620e6620547a
Benedict Aquino <benaq9@gmail.com>
2019-06-22 16:25:16 -0400

    String Slices
    rewrote `first_word` function to return a slice

--- 04_understanding_ownership/04-03_the_slice_type/src/main.rs
+++ 04_understanding_ownership/04-03_the_slice_type/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let s = String::from("hello world");
-
-    let len = s.len();
-
-    let slice = &s[0..len];
-    let slice = &s[..];
+    let some_string = String::from("hello world");
+    let word = first_word(&some_string);
+    println!("The first word in '{}' is '{}'.", some_string, word);
 }
 
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
