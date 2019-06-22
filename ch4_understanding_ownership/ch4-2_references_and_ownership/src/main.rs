f0c1ee8b325ea46bb62bc9a70d2a9e852430d6db
Benedict Aquino <benaq9@gmail.com>
2019-06-22 15:04:36 -0400

    Mutable References
    a reference's scope starts from where it is introduced and continues
    through the last time the reference is used
    
    the scopes of `r1` and `r2` end after the `println!` where they are last
    used, which is before the mutable reference `r3` is created. These
    scopes don't overlap, so this code is allowed

--- ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
+++ ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &s;     // no problem
-    let r2 = &s;     // no problem
-    let r3 = &mut s; // BIG PROBLEM
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{}, {}", r1, r2);
+    // r1 and r2 are no longer used after this point
 
-    println!("{}, {}, {}", r1, r2, r3);
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
 
