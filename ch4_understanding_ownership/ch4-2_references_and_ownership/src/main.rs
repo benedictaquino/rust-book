09a068c31430cd00688a67255e47d6473f7e217b
Benedict Aquino <benaq9@gmail.com>
2019-06-22 15:02:24 -0400

    Mutable References
    cannot borrow `s` as mutable because it is already borrowed as immutable

--- ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
+++ ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
@@ -1,11 +1,10 @@
-#![allow(unused_variables)]
 fn main() {
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-    }
+    let r1 = &s;     // no problem
+    let r2 = &s;     // no problem
+    let r3 = &mut s; // BIG PROBLEM
 
-    let r2 = &mut s;
+    println!("{}, {}, {}", r1, r2, r3);
 }
 
