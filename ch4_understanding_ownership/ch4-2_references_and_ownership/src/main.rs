f7cc9f3f468fdf34ada8e67c9602024fc0247504
Benedict Aquino <benaq9@gmail.com>
2019-06-22 15:00:06 -0400

    Mutable References
    create an inner scope to allow for multiple mutable references to `s`

--- ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
+++ ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
@@ -1,9 +1,11 @@
+#![allow(unused_variables)]
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    {
+        let r1 = &mut s;
+    }
 
-    println!("{}, {}", r1, r2);
+    let r2 = &mut s;
 }
 
