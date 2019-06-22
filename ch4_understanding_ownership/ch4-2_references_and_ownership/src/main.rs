c4151db6120dd64ea6f1b767be1316f7eb0c7466
Benedict Aquino <benaq9@gmail.com>
2019-06-22 14:49:25 -0400

    References and Ownership
    Listing 4-6: Attempting to modify a borrowed value

--- ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
+++ ch4_understanding_ownership/ch4-2_references_and_ownership/src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}", s1, len);
+    change(&s);
 }
 
-fn calculate_length(s: &String) -> usize { // s is a reference to a String
-    s.len()
-} // Here, s goes out of scope. But because it does not have ownership of what
-  // it refers to, nothing happens.
+fn change(some_string: &String) {
+    some_string.push_str(", world");
+}
+
