157512c66528d44d456f5c1ed77f1772e2cec09c
Benedict Aquino <benaq9@gmail.com>
2019-06-23 12:06:29 -0400

    Defining and Instantiating Structs
    Listing 5-3: Changing the value in the `email` field of a `User`
    instance

--- 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
+++ 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1
     };
+
+    user1.email = String::from("anotheremail@example.com");
 }
 
 struct User {
