97c313896184e7b6e58cad3a6d778f660d28592b
Benedict Aquino <benaq9@gmail.com>
2019-06-23 12:13:24 -0400

    Creating Instances from Other Instances With Struct Update Syntax
    Listing 5-6: Creating a new `User` instance using some of the values
    from `user1`

--- 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
+++ 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let mut user1 = User {
+    let user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1
     };
-
-    user1.email = String::from("anotheremail@example.com");
+    
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count 
+    };
 }
 
 struct User {
