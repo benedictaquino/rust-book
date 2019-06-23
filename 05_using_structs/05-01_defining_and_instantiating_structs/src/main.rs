2a5f64d9e24c5ca29e549d916e0050b17d084e78
Benedict Aquino <benaq9@gmail.com>
2019-06-23 12:04:54 -0400

    Defining and Instantiating Structs
    Listing 5-2: Creating an instance of the `User` struct

--- 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
+++ 05_using_structs/05-01_defining_and_instantiating_structs/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1
+    };
 }
 
 struct User {
