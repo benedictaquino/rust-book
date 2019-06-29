1773986755d2f5579c5bfad78f43d240c978312b
Benedict Aquino <benaq9@gmail.com>
2019-06-28 21:29:52 -0400

    Listing 5-12 with pretty print

--- 05_using_structs/05-02_an_example_program_using_structs/src/main.rs
+++ 05_using_structs/05-02_an_example_program_using_structs/src/main.rs
@@ -7,7 +7,7 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("rect1 is {:?}", rect1);
+    println!("rect1 is {:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
