ce078617e1eec62c4ad707230bde4bbd275ea08f
Benedict Aquino <benaq9@gmail.com>
2019-06-28 22:26:57 -0400

    Enum Values
    Listing 6-2: A `Message` enum whose variants each store different
    amounts and types of values

--- 06_enums/06-01/src/main.rs
+++ 06_enums/06-01/src/main.rs
@@ -3,6 +3,13 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
 
