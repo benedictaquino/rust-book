d2cc5f70c9609f7a6225c3b726fb5219f54905fc
Benedict Aquino <benaq9@gmail.com>
2019-06-28 22:11:01 -0400

    Enum Values
    creating instances of the two variants of `IpAddrKind`
    
    defining a function takes any `IpAddrKind`
    
    calling this function with either variant

--- 06_enums/06-01/src/main.rs
+++ 06_enums/06-01/src/main.rs
@@ -3,5 +3,13 @@ enum IpAddrKind {
     V6
 }
 
+fn route(ip_kind: IpAddrKind) { }
+
 fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
+
