f4bced3cf3c8986de08191a2789790c94ded0d64
Benedict Aquino <benaq9@gmail.com>
2019-06-28 21:26:03 -0400

    Adding Useful Functionality with Derived Traits
    Listing 5-11: Attempting to print a `Rectangle` instance

--- 05_using_structs/05-02_an_example_program_using_structs/src/main.rs
+++ 05_using_structs/05-02_an_example_program_using_structs/src/main.rs
@@ -6,10 +6,7 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("rect1 is {}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
