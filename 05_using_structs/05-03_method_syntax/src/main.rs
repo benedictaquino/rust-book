aa156671e0cd78a3ac4729bac5a1bc86fd5d97be
Benedict Aquino <benaq9@gmail.com>
2019-06-28 21:57:07 -0400

    Multiple `impl` Blocks
    Listing 5-16: Rewriting Listing 5-15 using multiple `impl` blocks

--- 05_using_structs/05-03_method_syntax/src/main.rs
+++ 05_using_structs/05-03_method_syntax/src/main.rs
@@ -8,19 +8,26 @@ impl Rectangle {
     fn square(size: u32) -> Rectangle {
         Rectangle{ width: size, height: size }
     }
+}
 
+impl Rectangle {
     fn area(&self) -> u32 {
         self.height * self.width 
     }
+}
 
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
-    let square1 = Rectangle::square(100);
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!("square1 is {:#?}", square1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
