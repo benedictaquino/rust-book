a9c5f3a381668b1e8d7022fb3df9166d2af44285
Benedict Aquino <benaq9@gmail.com>
2019-06-28 22:44:07 -0400

    Enum Values
    implemented a `print` method

--- 06_enums/06-01/src/main.rs
+++ 06_enums/06-01/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
@@ -6,13 +7,19 @@ enum Message {
 }
 
 impl Message {
-    fn call(&self) {
-        // method body would be defined here
+    fn print(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Move{ x: x1, y: y1 } => println!("Move: {}, {}", x1, y1),
+            Message::Write(msg) => println!("The message says: {}'", msg),
+            Message::ChangeColor(r, g, b) =>
+                println!("ChangeColor: {}, {}, {}", r, g, b),
+        }
     }
 }
 
 fn main() {
     let m = Message::Write(String::from("hello"));
-    m.call();
+    m.print();
 }
 
