208415daa720a0c6aa6807fa0fa29dbf62e770ed
Benedict Aquino <benaq9@gmail.com>
2019-07-06 16:46:14 -0400

    Pattern that Bind to Values
    Listing 6-4: A `Coin` enum in which the `Quarter` variant also holds a
    `UsState` value

--- 06_enums/06-02/src/main.rs
+++ 06_enums/06-02/src/main.rs
@@ -1,8 +1,62 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+    Nevada,
+    NewHampshire,
+    NewJersey,
+    NewMexico,
+    NewYork,
+    NorthCarolina,
+    NorthDakota,
+    Ohio,
+    Oklahoma,
+    Oregon,
+    Pennsylvania,
+    RhodeIsland,
+    SouthCarolina,
+    SouthDakota,
+    Tennessee,
+    Texas,
+    Utah,
+    Vermont,
+    Virginia,
+    Washington,
+    WestVirginia,
+    Wisconsin,
+    Wyoming,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -10,7 +64,7 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => 25,
     }
 }
 
@@ -18,7 +72,7 @@ fn main() {
     let penny = Coin::Penny;
     let nickel = Coin::Nickel;
     let dime = Coin::Dime;
-    let quarter = Coin::Quarter;
+    let quarter = Coin::Quarter(UsState::NewJersey);
 
     println!("The value of a penny in cents is {}.", value_in_cents(penny));
     println!("The value of a nickel in cents is {}.", value_in_cents(nickel));
