572da57b843cde2453f62a7d2b333cb6399148f0
Benedict Aquino <benaq9@gmail.com>
2019-07-06 16:56:01 -0400

    The `_` Placeholder
    the `_` pattern matches any value

--- 06_enums/06-02/src/main.rs
+++ 06_enums/06-02/src/main.rs
@@ -73,16 +73,18 @@ fn value_in_cents(coin: Coin) -> u8 {
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
+        None => None,
         Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    println!("five holds the value: {:?}", five);
-    println!("six holds the value: {:?}", six);
-    println!("none holds the value: {:?}", none);
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
